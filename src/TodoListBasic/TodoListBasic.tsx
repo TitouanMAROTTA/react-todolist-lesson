0baf8370a14c30c3ac36cd1fc46cfb8a9b2ac1e4
TitouanM <titouan.marotta@gmail.com>
2023-01-19 16:06:10 +0100

    add : handleChangeLastName/FirstName/Age

--- src/TodoListBasic/TodoListBasic.tsx
+++ src/TodoListBasic/TodoListBasic.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState  } from 'react';
 
 const TodoListBasic = () => {
-    const LastName = 'Titouan';
-    const FirstName = 'MAROTTA';
     
+    const [lastName, setLastName] = useState<string>('');
+    const [firstName, setFirstName] = useState<string>('');
+    const [age, setAge] = useState<number>();
+
+
+    const handleChangeLastName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setLastName(e.target.value)
+    }
+
+    const handleChangeFirstName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFirstName(e.target.value)
+    }
+
+    const handleChangeAge = (e: React.ChangeEvent<HTMLInputElement>) => {
+        // setAge(e.target.value)
+    }
+
+
+
     return <>
-        <div>{LastName}</div>
-        <div>{FirstName}</div>
+        <input type="text" onChange={handleChangeLastName} value={lastName}/>
+        <input type="text" onChange={handleChangeFirstName} value={firstName}/>
+        <input type="number" onChange={handleChangeAge} value={age}/>
+
+        <div>{lastName}</div>
+        <div>{firstName}</div>
+        <div>{age}</div>
     </>
+
 };
 
 export default TodoListBasic;
